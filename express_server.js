804ad406712e134b3f7c2e6e2b316e5644e23bf5
zinat <zinat.gholami@gmail.com>
2021-11-09 21:01:38 +0000

    Redirect After Form Submission

--- express_server.js
+++ express_server.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const bodyParser = require("body-parser");
 const app = express();
 const PORT = 8080; // default port 8080
 
@@ -9,57 +10,65 @@ const urlDatabase = {
   "9sm5xK": "http://www.google.com"
 };
 
-const bodyParser = require("body-parser");
 app.use(bodyParser.urlencoded({extended: true}));
 
 app.get("/", (req, res) => {
   res.send("Hello!");
 });
+
 app.get("/urls.json", (req, res) => {
-    res.json(urlDatabase);
-  });
+  res.json(urlDatabase);
+});
 
 app.get("/hello", (req, res) => {
-    res.send("<html><body>Hello <b>World</b></body></html>\n");
-  });
-  app.get("/set", (req, res) => {
-    const a = 1;
-    res.send(`a = ${a}`);
-   });
-   
-   app.get("/fetch", (req, res) => {
-    res.send(`a = ${a}`);
-   });
+  res.send("<html><body>Hello <b>World</b></body></html>\n");
+});
 
-   app.get("/urls/new", (req, res) => {
-    res.render("urls_new");
-  });
+app.get("/set", (req, res) => {
+  const a = 1;
+  res.send(`a = ${a}`);
+});
+  
+app.get("/fetch", (req, res) => {
+  res.send(`a = ${a}`);
+});
 
-   app.get("/urls", (req, res) => {
-    const templateVars = { urls: urlDatabase };
-    res.render("urls_index", templateVars);
-  });
+app.get("/urls/new", (req, res) => {
+  res.render("urls_new");
+});
 
-  app.get("/urls/:shortURL", (req, res) => {
-    const templateVars = { shortURL: req.params.shortURL, longURL: req.params.longURL/* What goes here? */ };
-    res.render("urls_show", templateVars);
-  });
+app.get("/urls", (req, res) => {
+  const templateVars = { urls: urlDatabase };
+  res.render("urls_index", templateVars);
+});
 
-  app.post("/urls", (req, res) => {
-    console.log(req.body);  // Log the POST request body to the console
-    res.send("Ok");         // Respond with 'Ok' (we will replace this)
-  });
+app.get("/urls/:shortURL", (req, res) => {
+  const templateVars = { shortURL: req.params.shortURL, longURL: req.params.shortURL/* What goes here? */ };
+  res.render("urls_show", templateVars);
+});
+
+app.post("/urls", (req, res) => {
+  console.log(req.body);  // Log the POST request body to the console
+  const longURL = req.body.longURL
+  const shortURL = generateRandomString()
+
+  urlDatabase[shortURL] = longURL
+
+  
+
+  res.redirect(`/urls/${shortURL}`)
+});
 
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
 });
 
-function generateRandomString($length = 10) {
-  $characters = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
-  $charactersLength = strlen($characters);
-  $randomString = '';
-  for ($i = 0; $i < $length; $i++) {
-      $randomString .$characters[rand(0, $charactersLength - 1)];
+function generateRandomString() {
+  const characters = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
+  let randomString = '';
+  for ($i = 0; $i < 6; $i++) {
+    const randomNum = Math.floor(Math.random() * characters.length)
+    randomString = randomString + characters[randomNum]     
   }
-  return $randomString;
+  return randomString;
 }
