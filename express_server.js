96ecb1c6f89270f401b6ffb2711d36897dbf8e46
zinat <zinat.gholami@gmail.com>
2021-11-09 23:58:07 +0000

    Update the Template and Routing on Server

--- express_server.js
+++ express_server.js
@@ -63,6 +63,12 @@ app.post("/urls", (req, res) => {
  
 });
 
+app.post("/urls/:shortURL/delete", (req, res) => {
+  const shortURL = req.params.shortURL;
+   delete urlDatabase[shortURL]
+   res.redirect("/urls")
+});
+
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
 });
