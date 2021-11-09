09eb59d098e553f95312f79d14e48cd54b14cbd6
zinat <zinat.gholami@gmail.com>
2021-11-09 13:50:31 +0000

    URL Shortening (Part 1)

--- express_server.js
+++ express_server.js
@@ -9,6 +9,9 @@ const urlDatabase = {
   "9sm5xK": "http://www.google.com"
 };
 
+const bodyParser = require("body-parser");
+app.use(bodyParser.urlencoded({extended: true}));
+
 app.get("/", (req, res) => {
   res.send("Hello!");
 });
@@ -28,16 +31,35 @@ app.get("/hello", (req, res) => {
     res.send(`a = ${a}`);
    });
 
+   app.get("/urls/new", (req, res) => {
+    res.render("urls_new");
+  });
+
    app.get("/urls", (req, res) => {
     const templateVars = { urls: urlDatabase };
     res.render("urls_index", templateVars);
   });
 
   app.get("/urls/:shortURL", (req, res) => {
-    const templateVars = { shortURL: req.params.shortURL, longURL: req.params.shortURL/* What goes here? */ };
+    const templateVars = { shortURL: req.params.shortURL, longURL: req.params.longURL/* What goes here? */ };
     res.render("urls_show", templateVars);
+  });
+
+  app.post("/urls", (req, res) => {
+    console.log(req.body);  // Log the POST request body to the console
+    res.send("Ok");         // Respond with 'Ok' (we will replace this)
   });
 
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
 });
+
+function generateRandomString($length = 10) {
+  $characters = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
+  $charactersLength = strlen($characters);
+  $randomString = '';
+  for ($i = 0; $i < $length; $i++) {
+      $randomString .$characters[rand(0, $charactersLength - 1)];
+  }
+  return $randomString;
+}
