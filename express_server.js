a51d77e4825cb57c6b7b65a5f55e0f5a83dc5b63
zinat <zinat.gholami@gmail.com>
2021-11-15 01:18:14 +0000

    add screenshots of the TinyApp project

--- express_server.js
+++ express_server.js
@@ -70,22 +70,9 @@ app.get("/urls.json", (req, res) => {
 app.get("/hello", (req, res) => {
   res.send("<html><body>Hello <b>World</b></body></html>\n");
 });
-
-/*app.get("/set", (req, res) => {
-  const a = 1;
-  res.send(`a = ${a}`);
-});
-
-app.get("/fetch", (req, res) => {
-  const a = 1;
-  res.send(`a = ${a}`);
-});*/
  
-
 app.get("/urls", (req, res) => {
   const userId = req.session.userId;
-  
-
   if (userId) {
     const userOBJ = users[`${userId}`];
     const templateVars = {
